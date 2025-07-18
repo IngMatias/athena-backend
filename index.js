8b4653f80636bd659563fd6ee6ddca6c09b2a248
Matias <matias.hernandez@correo.ucu.edu.uy>
2025-07-18 19:14:36 -0300

    .

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.use(cookieParser());
 app.use(
   rateLimit({
     windowMs: 15 * 60 * 1000,
-    max: 100,
+    max: 1000,
     standardHeaders: true,
     legacyHeaders: false,
     message: {
@@ -41,6 +41,11 @@ app.use(
     },
   })
 );
+
+app.use((req, res, next) => {
+  console.log(`[${new Date().toISOString()}] ${req.method} ${req.originalUrl}`);
+  next();
+});
 
 app.use("/api", routes);
 initWebSocket(server);
