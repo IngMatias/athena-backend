0b894d1be990458dd369cef46ff051a48ff111a3
Matias <matias.hernandez@correo.ucu.edu.uy>
2025-09-14 02:27:13 -0300

    fix/logs for html

--- src/services/video.service.js
+++ src/services/video.service.js
@@ -1,39 +1,45 @@
 // For xml parsing
-import { parseString } from 'xml2js';
+import { parseString } from "xml2js";
 
-import { getCaptionsMessages } from '../prompts/video.prompt.js';
+import { getCaptionsMessages } from "../prompts/video.prompt.js";
 
 import openaiExternal from "../external/chatgpt.external.js";
-import { timeToSeconds } from '../utils/time.util.js';
+import { timeToSeconds } from "../utils/time.util.js";
 
 export const isValid = (url) => {
-  const regexYoutubeURL = /^(https?:\/\/)?(www\.)?(youtube\.com\/watch\?v=|youtu\.be\/)([a-zA-Z0-9_-]{11})(\S*)?$/
-  return regexYoutubeURL.test(url)
-}
+  const regexYoutubeURL =
+    /^(https?:\/\/)?(www\.)?(youtube\.com\/watch\?v=|youtu\.be\/)([a-zA-Z0-9_-]{11})(\S*)?$/;
+  return regexYoutubeURL.test(url);
+};
 
 export const getVideoId = (url) => {
-  const urlObj = new URL(url)
+  const urlObj = new URL(url);
 
-  const searchParamId = urlObj.searchParams.get('v')
+  const searchParamId = urlObj.searchParams.get("v");
   if (searchParamId) {
-    return searchParamId
+    return searchParamId;
   }
 
-  const pathnameId = urlObj.pathname.substring(1)
+  const pathnameId = urlObj.pathname.substring(1);
   if (pathnameId) {
-    return pathnameId
+    return pathnameId;
   }
-}
+};
 
-export const getInfo = ({ url }) => {
+export const getInfo = async ({ url }) => {
+  console.log(url);
   return fetch(url)
-    .then(res => res.text())
-    .then(html => {
-      const title = html.match(/<title>(.*?)<\/title>/)[1]
-      const description = html.match(/"shortDescription":"(.*?)"/)[1].replace(/\\n/g, '\n').replace(/\\"/g, '"')
-      return { title, description }
-    })
-}
+    .then((res) => res.text())
+    .then((html) => {
+      console.log("html", html);
+      const title = html.match(/<title>(.*?)<\/title>/)[1];
+      const description = html
+        .match(/"shortDescription":"(.*?)"/)[1]
+        .replace(/\\n/g, "\n")
+        .replace(/\\"/g, '"');
+      return { title, description };
+    });
+};
 
 export const getCaptions = async ({ url }) => {
   const response = await fetch(process.env.DUMPLINGAI_URL, {
@@ -60,11 +66,11 @@ export const getCaptions = async ({ url }) => {
   if (transcript) {
     const lines = transcript.split("\n").filter(Boolean);
 
-    const parsed = lines.map(line => {
+    const parsed = lines.map((line) => {
       const [time, text] = line.split(" - ");
       return {
         start: timeToSeconds(time),
-        text: text.trim()
+        text: text.trim(),
       };
     });
 
@@ -72,11 +78,10 @@ export const getCaptions = async ({ url }) => {
       const next = parsed[index + 1];
       return {
         ...item,
-        duration: next ? next.start - item.start : null
+        duration: next ? next.start - item.start : null,
       };
     });
-
   }
 
   return transcript;
-}
+};
